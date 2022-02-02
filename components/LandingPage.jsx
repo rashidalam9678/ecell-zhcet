a859f2e71468fcca5dab3956cd95c36caf17e05b
rashidalam9678 <mohdrashidalam786@gmail.com>
2022-02-02 22:06:39 +0530

    Added some global css

---
+++ components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/LandingPage.module.css'
+
+const LandingPage = () => {
+  return (
+    <div className={styles.landing_page}>
+        <h1>this is tha landing page</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
