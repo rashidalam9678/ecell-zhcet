0dcff024660ce0e7a3e65b0d0cb0ad7c8527eb94
rashidalam9678 <mohdrashidalam786@gmail.com>
2022-11-03 23:27:07 +0530

    testing changes in razorpay

--- pages/member/payment/index.js
+++ pages/member/payment/index.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import { useEffect } from "react";
 
 const Payment = () => {
+    useEffect(()=>{
+        async function makeForm() {
+          const Script = document.createElement("script");
+          const Form = document.getElementById('rzp_payment_form');
+          Script.setAttribute('src',"https://checkout.razorpay.com/v1/payment-button.js" )
+          Script.setAttribute('data-payment_button_id',"pl_KbdFppFpEuSFvJ")
+          Form.appendChild(Script); 
+        }
+        makeForm()},[])
   return (
     <div className=''>
         <h1>  I am the payment page</h1>
-        
-        <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_KbdFppFpEuSFvJ" async> </script> </form>
+
+        <form id="rzp_payment_form"></form>
 
     </div>
   )
 }
 
 export default Payment
+
